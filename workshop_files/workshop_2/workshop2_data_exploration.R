ac11b7c0b8ca76f1f173c55d517e53df57fd9ead
Tawfiq Keskas <zidan40o0@hotmail.com>
2021-02-11 17:39:09 +0100

    added workshop 2 notes

---
+++ workshop_files/workshop_2/workshop2_data_exploration.R
@@ -0,0 +1,100 @@
+#start by doing the homework####
+#Homework Assignment#
+# 1- Make a new project
+# 2- Pull the main git branch into your computer
+# 3- Create a new branch in your repo and name it myname_homework1_sol
+# 4- You see the file named "supermarket_sales - Sheet1.csv" in R, rename it according to the good practice we discussed then 
+# import this file into a dataframe. 
+# 5- What is the class of this data and what is its disk size?
+# 6- Does this data have any factor variables? 
+# 7- How many rows and columns it has?
+# 8- Give basic statistics on the data at hand
+# 9- Does it have any empty values? If so, where?
+# 10- Explore the grepl() function, what does it do? (Please use the built-in documentation system by typing ?grepl() )
+# 
+# Bonus 1: 
+#   Give the summary statistics of the first 100 rows for the unit_price column. (default ordering) 
+# 
+# Bonus 2: 
+#   Illustrate at least three different methods to get data only from the seventh column 
+# 
+# Extra Bonus 3: 
+#   Keep only the rows that have Rating lower than the median 
+# 
+# Final Task:
+#   Save, Commit and Push your script to your remote github repo with the comment "Done!". 
+
+#Talk about R packages####
+#what is a package? 
+#the tidyverse which contains: 
+ #dplyr 
+ #ggplot2 
+ #lubridate 
+ #purrr 
+#and much more 
+
+#fliptime
+#reshape2
+#RPostgres
+#keyring 
+#jsonlite
+#httr 
+
+#mlr
+#h2o
+
+#how to install and load a package 
+
+
+
+
+#freestyle exploration of the supermarket data #### 
+ #explain the meaning of ID's and the importance of unique() 
+ #look at the columns one by one 
+ #do str() and explain why character isn't good 
+ #notice the date
+ #do a factor and also a date conversion 
+ #sanity check the tax rate 
+
+#mutate()
+#filter()
+
+#dates and group by####
+ #convert to weeks 
+ #convert to month 
+ #convert to days
+ #convert to years 
+ #group by year, day, week, month 
+
+#count number of members and non members for each gender 
+
+
+View(df)
+
+nrow(df) #we have 1000 rows 
+length(unique(df$`Invoice ID`)) #we indeed have 1000 unique IDs
+
+the_unique_ids <- 
+  unique(df$`Invoice ID`)
+
+the_unique_ids
+
+x <- 
+  c("a", "a", "b", "b", "c", "d")
+
+length(unique(x))
+
+unique(df$Branch)
+
+length(unique(df$Branch))
+
+unique(df$City)
+
+unique(df$`Customer type`)
+unique(df$`Product line`)
+unique(df$Payment)
+
+summary(df)
+str(df)
+
+summary(df$Branch)
