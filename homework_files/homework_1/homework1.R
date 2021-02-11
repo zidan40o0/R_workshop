2cb1a72046cea7db339206d0ad7cae4c51a567d2
Tawfiq Keskas <zidan40o0@hotmail.com>
2021-02-11 12:55:00 +0100

    added the homework file

---
+++ homework_files/homework_1/homework1.R
@@ -0,0 +1,31 @@
+#Homework Assignment#
+# 1- Make a new project
+# 2- Pull the main git branch into your computer
+# 3- Create a new branch in your repo and name it myname_homework1_sol
+# 4- You see the file named "supermarket_sales - Sheet1.csv" in R, rename it according to the good practice we discussed then 
+# import this file into a dataframe. 
+# 5- What is the class of this data and what is its disk size?
+# 6- Does this data have any factor variables? 
+# 7- How many rows and columns it has?
+# 8- Give basic statistics on the data at hand
+# 9- Does it have any empty values? If so, where?
+# 10- Explore the grepl() function, what does it do? (Please use the built-in documentation system by typing ?grepl() )
+# 
+# Bonus 1: 
+#   Give the summary statistics of the first 100 rows for the unit_price column. (default ordering) 
+# 
+# Bonus 2: 
+#   Illustrate at least three different methods to get data only from the seventh column 
+# 
+# Extra Bonus 3: 
+#   Keep only the rows that have Rating lower than the median 
+# 
+# Final Task:
+#   Save, Commit and Push your script to your remote github repo with the comment "Done!". 
+# 
+# 
+# Please remember to write proper comments and documentation when working on your scripts. 
+# 
+# I will correct everything on next week's session.
+# 
+# (this is the link for how to clone my remote repo to your own : https://stackoverflow.com/questions/18200248/cloning-a-repo-from-someone-elses-github-and-pushing-it-to-a-repo-on-my-github) 
