8d1927ae7b25d2e668535fd5026edbc8ac5863e4
Tawfiq Keskas <zidan40o0@hotmail.com>
2021-02-18 19:50:07 +0100

    added workshop 3 work notes

---
+++ workshop_files/workshop_3/workshop_3.R
@@ -0,0 +1,11 @@
+#We begin by doing the homework from Workshop 2####
+
+#SQL and keyring####
+#What is SQL (with focus on pSQL)
+ #Introductory theory 
+ #Why using Excel as a database is dumb (mostly)
+ #Live example with AACT database and how to connect to it using DBeaver 
+ #SELECT, JOIN, GROUP BY, DISTINCT, etc etc (cover all of these) 
+
+#How to connect safely with the use of keyring package? (so that you don't expose your credentials)
+#How to use the PostgreSQL package in R and what could be some issues you face 
