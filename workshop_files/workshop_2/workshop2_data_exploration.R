b165218f0cdbb6a5a3a66e1a8807fff532095057
Tawfiq Keskas <zidan40o0@hotmail.com>
2021-03-11 13:52:12 +0100

    testing forking

--- workshop_files/workshop_2/workshop2_data_exploration.R
+++ workshop_files/workshop_2/workshop2_data_exploration.R
@@ -1,7 +1,15 @@
 library(readr)
 library(tidyverse)
-df2 <- 
-  read_csv("workshop_files/workshop_2/supermarket_sales - Sheet1.csv")
+df1 <- 
+  read_csv("workshop_files/workshop_2/supermarket_sales_sheet1.csv")
+
+class(df1)
+str(df1)
+nrow(df1)
+ncol(df1)
+summary(df1)
+sum(is.na(df1))
+?grepl()
 #start by doing the homework####
 #Homework Assignment#
 # 1- Make a new project
@@ -21,7 +29,6 @@ df2 <-
 # 
 # Bonus 2: 
 #   Illustrate at least three different methods to get data only from the seventh column 
-# 
 # Extra Bonus 3: 
 #   Keep only the rows that have Rating lower than the median 
 # 
@@ -49,7 +56,11 @@ df2 <-
 
 #how to install and load a package 
 
+install.packages("keyring")
+library(keyring)
 
+install.packages("tidyverse")
+library(tidyverse)
 #freestyle exploration of the supermarket data #### 
  #explain the meaning of ID's and the importance of unique() 
  #look at the columns one by one 
@@ -57,17 +68,57 @@ df2 <-
  #notice the date
  #do a factor and also a date conversion 
  #sanity check the tax rate 
+View(df1)
 
+nrow(df1) == sum(!duplicated(df1$`Invoice ID`))
+
+length(unique(df1$`Invoice ID`))
+
+length(unique(df1$`Product line`))
+
+unique(df1$`Product line`)
+unique(df1$City)
+
+unique(df1[,c("City", "Branch")])
+unique(df1[,c("City", "Branch")])
+
+#testing for similarity in gross margin and tax (probable error)
+
+sum(!(df1$`Tax 5%` == df1$`gross margin percentage`))
+
+sum((df1$`Tax 5%` == df1$`gross income`)) == 1000 #then a problem
+sum(df1$`Tax 5%` == df1$`gross income`) != 0  #we have a problem 
 #mutate()
 #filter()
 
 #dates and group by####
  #convert to weeks 
 
+str(df1)
+df1_clean <- 
+  df1 %>% #from magrittr package
+  mutate(., clean_date = mdy(Date)) %>% 
+  filter(., clean_date < ymd("2019-03-08")) %>% 
+  filter(., Gender == "Male") %>% 
+  mutate(., testing_tax1 = 0.05*Total, 
+            test_result = (testing_tax1 == `Tax 5%`), 
+            test2 = ((`gross margin percentage` * Total) == `Tax 5%`))
 
-mon_num <- 
-  month(mdy(df2$Date), label = T, abbr = T)
 
+df1_clean <- 
+  df1 %>% #from magrittr package
+  mutate(., clean_date = mdy(Date))
+
+View(df1_clean)
+df1_clean <- 
+  df1 %>% 
+  mutate(., clean_date = mdy(Date)) %>% 
+  mutate(., the_months = month(clean_date, label = T, abbr = F), 
+            the_days = wday(clean_date, label = T, abbr = F)) %>% 
+  group_by(., the_months, Gender) %>% 
+  summarize(., sales_per_gender_per_month = sum(Total))
+
+df1_clean
 
  #convert to month 
  #convert to days
@@ -77,10 +128,11 @@ mon_num <-
 #count number of members and non members for each gender 
 library(tidyverse)
 test <- 
-  df2 %>% 
+  df1 %>% 
   group_by(., Gender) %>% 
   count(`Customer type`)
 
+test
 test
 
 View(df)
